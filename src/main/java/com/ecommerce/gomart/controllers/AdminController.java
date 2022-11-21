126871aa905aee830cb7563a73d260472dd513c3
majimearun <arunachala0619@gmail.com>
2022-11-21 11:37:05 +0530

    contorllers for admin and manager done

--- src/main/java/com/ecommerce/gomart/controllers/AdminController.java
+++ src/main/java/com/ecommerce/gomart/controllers/AdminController.java
@@ -1,4 +1,42 @@
 package com.ecommerce.gomart.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.gomart.models.Product;
+import com.ecommerce.gomart.services.AdminService;
+
+@RestController
+@RequestMapping(path = "/api/v1/admin")
 public class AdminController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService){
+        this.adminService = adminService;
+    }
+
+    @PostMapping(path = "/addManager")
+    public void addManager(@RequestBody Long userId){
+        adminService.giveManagerAccess(userId);
+    }
+
+    @PostMapping(path = "/addProduct")
+    public void addProduct(@RequestBody Product product){
+        adminService.addProduct(product);
+    }
+
+    @PostMapping(path = "/updateProduct")
+    public void updateProduct(@RequestBody Product product){
+        adminService.updateProduct(product);
+    }
+
+    @PostMapping(path = "/deleteProduct")
+    public void deleteProduct(@RequestBody Long id){
+        adminService.deleteProduct(id);
+    }
 }
--- src/main/java/com/ecommerce/gomart/controllers/ManagerController.java
+++ src/main/java/com/ecommerce/gomart/controllers/ManagerController.java
@@ -1,4 +1,37 @@
 package com.ecommerce.gomart.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.gomart.models.Product;
+import com.ecommerce.gomart.services.ManagerService;
+
+@RestController
+@RequestMapping(path = "/api/v1/manager")
 public class ManagerController {
+    private final ManagerService managerService;
+
+    @Autowired
+    public ManagerController(ManagerService managerService){
+        this.managerService = managerService;
+    }
+
+    @PostMapping(path = "/addProduct")
+    public void addProduct(@RequestBody Product product){
+        managerService.addProduct(product);
+    }
+
+    @PostMapping(path = "/updateProduct")
+    public void updateProduct(@RequestBody Product product){
+        managerService.updateProduct(product);
+    }
+
+    @PostMapping(path = "/deleteProduct")
+
+    public void deleteProduct(@RequestBody Long id){
+        managerService.deleteProduct(id);
+    }
 }
